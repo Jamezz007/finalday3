783a48146318fb2f56392d21fac5deb5c8de6261
James <jamesbabu20000@gmail.com>
2024-09-23 15:44:16 +0530

    first commit

--- nmng.py
+++ nmng.py
@@ -1,10 +1,13 @@
-import math
+import random
 
-a=math.pow(2,5)
-floorvar=math.floor(6.9)
-ceilvar=math.ceil(6.9)
-fabsvar=math.fabs(98.5)
-infinityvar=math.inf
-print(floorvar)
-print(ceilvar)
-print(fabsvar)
+random_variable = random.random()
+random_init_variable = random.randint(111111,999999)
+random_range_variable = random.randrange(100,1000,2)
+uniform_random_variable = random.uniform(3,5)
+
+sample_list = [1,2,345,6]
+choice_variable = random.choice(sample_list)
+random.shuffle(sample_list)
+
+print(sample_list)
+
